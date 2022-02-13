684839f955e8343fbf9cbba376907d6e1707306b
Damian Jansen <damian.jansen@gmail.com>
2022-02-13 17:31:32 +1000

    Fix src/sensors/test/sensors.test.ts

--- src/sensors/test/sensors.test.ts
+++ src/sensors/test/sensors.test.ts
@@ -8,7 +8,7 @@ describe('Sensor tests', () => {
 
   it ('throws an error on invalid dht11/22 sensor type', async () => {
     try {
-      await dhtxxTemp(99, '1000', )
+      await dhtxxTemp(99, '1000')
       throw Error('(Control) Failed to catch the error')
     } catch(err) {
       expect(err.message).toBe('Invalid sensor type 99: 11 or 22')
