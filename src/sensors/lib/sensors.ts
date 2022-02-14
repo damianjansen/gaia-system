6c7d5628800f52b99d52d4913546045226589632
damianjansen <damian.jansen@gmail.com>
2022-02-14 17:41:07 +1000

    Add testmode and fix stuff

--- src/sensors/lib/sensors.ts
+++ src/sensors/lib/sensors.ts
@@ -4,10 +4,15 @@ import 'dotenv/config'
 import { SensorData, SensorReading } from '../types/types'
 import { writeToLog } from '../../recorder/lib/recorder'
 
-/** Environment variables **/
-const mocksensors = process.env.MOCK_SENSORS
+/**
+ * Testmode provides a method to run the system without causing 
+ * damage to the local environment. It will prevent GPIO and file
+ * writing for devices.
+ * It should be off for 'yarn test'
+ */
+const testmode = true
 
-const HWSENSORPREFIX='/sys/bus/w1/devices/';
+const HWSENSORPREFIX=testmode?'/tmp/gpio':'/sys/bus/w1/devices/';
 
 
 //TODO: move to json
@@ -15,31 +20,37 @@ const HWSENSORPREFIX='/sys/bus/w1/devices/';
 const sensorArray = [
     {
         name: "Chamber",
-        location: "11",
+        location: "5",
         type: "DHT11",
         uuid: "Do these have one?"
     },
     {
         name: "Internal",
-        location: "21",
+        location: "6",
         type: "DHT11",
         uuid: "Do these have one?"
     },
     {
+      name: "Ambient",
+      location: "13",
+      type: "DHT11",
+      uuid: "Do these have one?"
+    },
+    {
         name: "Cauldron",
-        location: "1",
+        location: "26",
         type: "DS18B20",
         uuid: "alpha"
     },
     {
         name: "Canister",
-        location: "1",
+        location: "26",
         type: "DS18B20",
         uuid: "bravo"
     },
     {
         name: "Radiator",
-        location: "1",
+        location: "26",
         type: "DS18B20",
         uuid: "charlie"
     }
@@ -55,6 +66,10 @@ const sensorArray = [
 const ds18b20Temp = async (sensor: string): Promise<SensorReading> => {
   const ret: SensorReading = { temperature: '0', humidity: '0', error: null }
   const hwmonDir = `${HWSENSORPREFIX}/${sensor}/hwmon`
+  if (testmode) {
+    ret.temperature = (parseFloat(readFileSync(`${hwmonDir}/1/temp1_input`, 'utf-8'))/1000.0).toString()
+    return ret
+  }
   opendir(hwmonDir, async (err, dir) => {
     if (err) {
       ret.error = Error(`Failed to open dir ${err}`)
@@ -81,6 +96,11 @@ const ds18b20Temp = async (sensor: string): Promise<SensorReading> => {
 const dhtxxTemp = async (type: number, sensor: string): Promise<SensorReading> => {
   if (![11,22].includes(type)) throw Error(`Invalid sensor type ${type}: 11 or 22`)
   let ret: SensorReading = { temperature: '0', humidity: '0', error: null }
+  if (testmode) {
+    ret.temperature = readFileSync(`/tmp/gpio${sensor}temperature`, { encoding: 'utf-8' }).trim()
+    ret.humidity = readFileSync(`/tmp/gpio${sensor}humidity`, { encoding: 'utf-8' }).trim()
+    return ret
+  }
   read(type as SensorType, parseInt(sensor), (err, temperature: number, humidity: number) => {
     if (err) {
       ret.error = Error(`Failed to read from sensor on pin ${sensor}: ${err.message}`)
@@ -102,49 +122,24 @@ const dhtxxTemp = async (type: number, sensor: string): Promise<SensorReading> =
 const readSensors = async (): Promise<Array<SensorData>> => {
   const now = Date.now().valueOf()
   let sensorData: SensorData[] = []
-  if (mocksensors) {
-        // Pretend to read from all sensors
-    (await getMockData()).forEach(async (d) => {
-        sensorData.push(d)
-    })
-  } else {
-    sensorArray.forEach(async (sensorRead) => {
-      let response: SensorReading
-        if(sensorRead.type === 'DHT11' || sensorRead.type === 'DHT22') {
-          response = await dhtxxTemp(sensorRead.type === 'DHT11' ? 11 : 22, sensorRead.location)
-        } else if(sensorRead.type === 'DS18B20') {
-          response = await ds18b20Temp(sensorRead.uuid)
-        } else {
-          throw Error(`Unknown sensor type ${sensorRead.type}`)
-        }
+  sensorArray.forEach(async (sensorRead) => {
+    let response: SensorReading
+      if(sensorRead.type === 'DHT11' || sensorRead.type === 'DHT22') {
+        response = await dhtxxTemp(sensorRead.type === 'DHT11' ? 11 : 22, sensorRead.location)
+      } else if(sensorRead.type === 'DS18B20') {
+        response = await ds18b20Temp(sensorRead.uuid)
+      } else {
+        throw Error(`Unknown sensor type ${sensorRead.type}`)
+      }
 
-        if (!response.error) {
-          sensorData.push({ name: sensorRead.name, location: sensorRead.location, type: sensorRead.type, temperature: response.temperature, humidity: response.humidity, timestamp: now })
-        } else {
-          writeToLog(`[ERROR]: Failed to read from sensor: ${response.error.message}`)
-        }
-    })
-  }
+      if (!response.error) {
+        sensorData.push({ name: sensorRead.name, location: sensorRead.location, type: sensorRead.type, temperature: response.temperature, humidity: response.humidity, timestamp: now })
+      } else {
+        writeToLog(`[ERROR]: Failed to read from sensor: ${response.error.message}`)
+      }
+  })
   return sensorData
 }
 
-// Mock for testing
-const getMockData = async (): Promise<Array<SensorData>> => {
-    let sensorData: SensorData[] = []
-    const now = Date.now()
-    sensorArray.forEach(sensorRead => {
-        // Pretend to read from all sensors
-        if (sensorRead.type == 'DHT11' || sensorRead.type === 'DHT22') {
-            const response = { temperature: Math.floor(Math.random() * (30 - 20 + 1) + 20), humidity: Math.floor(Math.random() * (60 - 50 + 1) + 50)}
-            sensorData.push({ name: sensorRead.name, location: sensorRead.location, type: sensorRead.type, temperature: response.temperature.toString(), humidity: response.humidity.toString(), timestamp: now })
-        } else if (sensorRead.type == 'DS18B20') {
-            const response = { temperature: Math.floor(Math.random() * (30 - 20 + 1) + 20) }
-            sensorData.push({ name: sensorRead.name, location: sensorRead.location, type: sensorRead.type, temperature: response.temperature.toString(), humidity: '100', timestamp: now })
-        } else {
-            console.log(`Unknown sensor type: ${sensorRead.type}`)
-        }
-    })
-    return sensorData
-}
 
 export { readSensors, dhtxxTemp, ds18b20Temp }
